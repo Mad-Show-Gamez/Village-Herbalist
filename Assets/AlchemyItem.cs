91fa9c1331cdb2a3a9b08b4e547ac6aa009c99b5
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 01:40:55 +0100

    add asset menu reff add color

--- Assets/AlchemyItem.cs
+++ Assets/AlchemyItem.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[AddComponentMenu("alchemy/Item")]
 public class AlchemyItem : ScriptableObject
 {
     Sprite sprite;
     string itemName;
+    Color color;
     string description;
     Tag[] tags;
 }
