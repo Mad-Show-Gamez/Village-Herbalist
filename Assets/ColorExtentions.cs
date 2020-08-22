fe31477cd61af0490d0323210fa5d6fcc03ac208
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 20:00:07 +0100

    add extention methods for color and vectors

---
+++ Assets/ColorExtentions.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class ColorExtentions
+    {
+
+        public static Color SetR(this Color c, float v)
+        {
+            c.r = v;
+            return c;
+        }
+        public static Color SetG(this Color c, float v)
+        {
+
+            c.g = v;
+            return c;
+        }
+        public static Color SetB(this Color c, float v)
+        {
+            c.b = v;
+            return c;
+        }
+
+        public static Color SetA(this Color c, float v)
+        {
+            c.a = v;
+            return c;
+        }
+        /// <summary>
+        /// converts a color to hsv h=x s=y v=z w=a
+        /// </summary>
+        /// <param name="c">color to convert to HSV vector</param>
+        /// <returns></returns>
+        public static Vector4 ColorToHSV(this Color c)
+        {
+            Vector4 f = new Vector3();
+            Color.RGBToHSV(c, out f.x, out f.y, out f.z);
+            f.w = c.a;
+            return f;
+        }
+        public static Color avrageColor(this IEnumerable<Color> c)
+        {
+            var count = c.Count();
+            if (count < 1)
+                return new Color(0, 0, 0, 0);
+            return (c.Select(i => i.ColorToHSV()).Aggregate((c1, c2) => c1 + c2) * (1f / count)).ToColorHSV();
+        }
+        /// <summary>
+        /// converts color to a vector4 r=x g=y b=z a=w
+        /// </summary>
+        /// <param name="c">color to convert to RGB vector</param>
+        /// <returns></returns>
+        public static Vector4 ToVector(this Color c)
+        {
+            return new Vector4(c.r, c.g, c.b, c.a);
+        }
+    }
+}
---
+++ Assets/VectorExtentions.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class VectorExtentions
+    {
+        public static Color ToColorRGB(this Vector4 v)
+        {
+            return new Color(v.x, v.y, v.z, v.w);
+        }
+        public static Color ToColorRGB(this Vector3 v)
+        {
+            return new Color(v.x, v.y, v.z, 1);
+        }
+        public static Color ToColorHSV(this Vector4 v)
+        {
+            return Color.HSVToRGB(v.x, v.y, v.z).SetA(v.w);
+        }
+        public static Color ToColorHSV(this Vector3 v)
+        {
+            return Color.HSVToRGB(v.x, v.y, v.z);
+        }
+        public static Vector4 ScaleChain(this Vector4 t, Vector4 o)
+        {
+            return Vector4.Scale(t,o);
+        }
+        public static Vector3 ScaleChain(this Vector3 t, Vector3 o)
+        {
+            return Vector3.Scale(t, o);
+        }
+        public static Vector2 ScaleChain(this Vector2 t, Vector2 o)
+        {
+            return Vector2.Scale(t, o);
+        }
+        public static Vector4 setX(this Vector4 v, float value)
+        {
+            v.x = value;
+            return v;
+        }
+        public static Vector4 setY(this Vector4 v, float value)
+        {
+            v.y = value;
+            return v;
+        }
+        public static Vector4 setZ(this Vector4 v, float value)
+        {
+            v.z = value;
+            return v;
+        }
+        public static Vector4 setW(this Vector4 v, float value)
+        {
+            v.w = value;
+            return v;
+        }
+
+        public static Vector3 setX(this Vector3 v, float value)
+        {
+            return ((Vector4)v).setX(value);
+        }
+        public static Vector3 setY(this Vector3 v, float value)
+        {
+            return ((Vector4)v).setY(value);
+        }
+        public static Vector3 setZ(this Vector3 v, float value)
+        {
+            return ((Vector4)v).setZ(value);
+        }
+
+        public static Vector2 setX(this Vector2 v, float value)
+        {
+            return ((Vector4)v).setX(value);
+        }
+        public static Vector2 setY(this Vector2 v, float value)
+        {
+            return ((Vector4)v).setY(value);
+        }
+    }
+}
