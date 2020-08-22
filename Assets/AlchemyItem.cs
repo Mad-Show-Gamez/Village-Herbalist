da0302a5964f9af3ba37ced820e51eceb9e136e2
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 02:10:18 +0100

    add name sync add slug getter

--- Assets/AlchemyItem.cs
+++ Assets/AlchemyItem.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq
 
 [CreateAssetMenu(fileName = "New AlchemyItem", menuName = "Alchemy Item")]
 public class AlchemyItem : ScriptableObject
 {
     Sprite sprite;
-    string itemName;
+    string _itemName;
+    public string itemName
+    {
+        get
+        {
+            var name = this.itemName ?? _itemName;
+            _itemName = name;
+            return name;
+        }
+    }
+    public string slug { get => itemName.Replace(' ','_'); }
     string description;
     Tag[] tags;
 }
