5b35fa926cf1d15f012fa4cc6bc369cfccd15620
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 20:01:21 +0100

    add potion bottle

---
+++ Assets/ContainerCollect.cs
@@ -0,0 +1,34 @@
+﻿using Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ContainerCollect : MonoBehaviour, IColorProvider
+{
+    [SerializeField]
+    int capacity = 1;
+    [SerializeField]
+    AlchemyItem[] content;
+    Color currentColor = new Color(0, 0, 0, 0);
+
+    public Color CurrentColor => currentColor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tool = collision.GetComponent<IAlchemyTool>();
+        if (tool!=null)
+        {
+            if (tool.items.Count() <= capacity)
+            {
+                content = tool.Empty().ToArray();
+                currentColor = content.Select(i => i.Color).avrageColor();
+            }
+        }
+    }
+    private void OnValidate()
+    {
+
+        currentColor = content.Where(i=>i!=null).Select(i => i.Color).avrageColor();
+    }
+}
