c1ff21036235db8bac71c5c1573cb5bcbcc89d5b
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 02:41:04 +0100

    fix recursive property add serilize field to sprite

--- Assets/AlchemyItem.cs
+++ Assets/AlchemyItem.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New AlchemyItem", menuName = "Alchemy Item")]
 public class AlchemyItem : ScriptableObject
 {
+    [SerializeField]
     Sprite sprite;
     string _itemName;
     public string itemName
     {
         get
         {
-            var name = this.itemName ?? _itemName;
+            var name = this.name ?? _itemName;
             _itemName = name;
             return name;
         }
     }
     public string slug { get => itemName.Replace(' ','_'); }
     [SerializeField]
+    [NaughtyAttributes.ResizableTextArea]
     string description;
     [SerializeField]
     Color color= Color.white;
