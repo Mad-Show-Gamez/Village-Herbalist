87853346d7f9711de4375563abb3deab449fed54
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-22 01:10:47 +0100

    add Recipie singleton list all

--- Assets/Recipe.cs
+++ Assets/Recipe.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Recipe : ScriptableObject
 {
+    public static List<Recipe> all = new List<Recipe>();
     AlchemyItem output;
     AlchemyItem[] input;
+    public void OnDisable()
+    {
+        Recipe.all.Remove(this);
+    }
+    public void OnEnable()
+    {
+        Recipe.all.Add(this);
+    }
 }
