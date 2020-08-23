37cd341bab9b978187f6ca3c72f2518ae011cb12
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-23 01:14:13 +0100

    addd some genorator functions

--- Assets/Scripts/Scriptable objects/AlchemyItem.cs
+++ Assets/Scripts/Scriptable objects/AlchemyItem.cs
@@ -18,6 +18,8 @@ public class AlchemyItem : ScriptableObject
     Color color = Color.white;
     [SerializeField]
     Tag[] tags;
+    [SerializeField]
+    int value;
     public string itemName
     {
         get
@@ -32,7 +34,7 @@ public class AlchemyItem : ScriptableObject
     public string Description { get => description; private set => description = value; }
     public Color Color { get => color; private set => color = value; }
     public IEnumerable<Tag> Tags { get => tags; private set => tags = value.ToArray(); }
-  
+    public int Value { get => value; private set => this.value = value; }
 
     [MenuItem("Assets/Create/Alchemy Item/Plant/Leaf")]
     static void createSubLeaf()
@@ -85,19 +87,53 @@ public class AlchemyItem : ScriptableObject
     [MenuItem("Assets/Create/Alchemy Item/Animal/All")]
     static void createSubAllAnimal()
     {
+
         createSubItem("Heart");
         createSubItem("Eye");
         createSubItem("Blood");
         createSubItem("Brain");
+
     }
-    static void createSubItem(string item_type)
+    [MenuItem("Assets/Create/Alchemy Item/Animal/All With Tool")]
+    static void createSubAllAnimalWithTool()
     {
+        createSubAllWithTool(new string[] { "Heart", "Eye", "Blood", "Brain" });
+    }
+    [MenuItem("Assets/Create/Alchemy Item/Plant/All With Tool")]
+    static void createSubAllPlantWithTool()
+    {
+        createSubAllWithTool(new string[] { "Leaf", "Flower", "Fruit", "Seed" });
+    }
+    static void createSubAllWithTool(IEnumerable<string> types) { 
+        string path = EditorUtility.OpenFilePanel("Open Tool", "Scripptable Objects/Tools", "asset");
+        foreach (var parttype in types)
+            foreach (var part in createSubItem(parttype, path))
+            {
+                Recipe.CreateRecipyFrom(new Object[] { part.Item1, part.Item2 }, part.Item3);
+            }
+    }
+    static IEnumerable<(Object, Object, Object)> createSubItem(string item_type, string tool_path = null)
+    {
+        List<(Object, Object, Object)> newpaths = new List<(Object, Object, Object)>();
         foreach (var s in Selection.objects)
         {
             var path1 = AssetDatabase.GetAssetPath(s);
             var extention = Path.GetExtension(path1);
             var path2 = path1.Replace(extention, "_" + item_type + extention);
+            var fn = Path.GetFileName(path2);
+            path2 = path2.Replace(fn, "");
+            path2 = Path.Combine(path2, item_type, fn);
             AssetDatabase.CopyAsset(path1, path2);
+            if (tool_path == null)
+            {
+                return newpaths;
+            }
+            tool_path = tool_path.Substring(tool_path.IndexOf("Assets"));
+            var tool = AssetDatabase.LoadAssetAtPath(tool_path, typeof(AlchemyItem));
+            var ing = AssetDatabase.LoadAssetAtPath(path1, typeof(AlchemyItem));
+            var result = AssetDatabase.LoadAssetAtPath(path2, typeof(AlchemyItem));
+            newpaths.Add((tool, ing, result));
         };
+        return newpaths;
     }
 }
--- Assets/Scripts/Scriptable objects/Recipe.cs
+++ Assets/Scripts/Scriptable objects/Recipe.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "new Recipe", menuName = "Recipe")]
 public class Recipe : ScriptableObject
@@ -18,7 +19,7 @@ public class Recipe : ScriptableObject
         all.Add(this);
     }
 
-    public static AlchemyItem Craft(IEnumerable<AlchemyItem> inputItems, AlchemyItem RejectItem=null)
+    public static AlchemyItem Craft(IEnumerable<AlchemyItem> inputItems, AlchemyItem RejectItem = null)
     {
         var satisfy = all.Where(r =>
             r.input.Length == inputItems.Count() &&
@@ -27,5 +28,39 @@ public class Recipe : ScriptableObject
         int count = satisfy.Count();
         return count > 0 ? satisfy.ElementAt(Random.Range(0, count)).output : RejectItem;
 
+    }
+    [MenuItem("Assets/Create/Alchemy Item/Recipies/Create From...")]
+    static void CreateRecipyFrom()
+    {
+        string path = EditorUtility.OpenFilePanel("Open Result", "Scripptable Objects/Potions", "asset");
+        path = path.Substring(path.IndexOf("Assets"));
+        CreateRecipyFromSaveAs(Selection.objects, AssetDatabase.LoadAssetAtPath(path, typeof(AlchemyItem)));
+    }
+    public static void CreateRecipyFrom(IEnumerable<Object> objects, Object result)
+    {
+        var rname = System.String.Join("_", objects.Select(s => s.name.Replace(" ", "_"))) + "To" + result.name;
+        string path = System.IO.Path.Combine("Assets/Scripptable Objects/Recipys", objects.First().name, rname) + ".asset";
+        var dp = System.IO.Path.GetDirectoryName(path);
+        if (!AssetDatabase.IsValidFolder(dp))
+            AssetDatabase.CreateFolder("Assets/Scripptable Objects/Recipys", objects.First().name);
+
+        var r = ScriptableObject.CreateInstance<Recipe>();
+        r.input = objects.Select(s => s as AlchemyItem).ToArray();
+        r.output = result as AlchemyItem;
+        AssetDatabase.CreateAsset(r, path);
+    }
+    static void CreateRecipyFromSaveAs(IEnumerable<Object> objects, Object result)
+    {
+        var rname = System.String.Join("_", objects.Select(s => s.name.Replace(" ", "_"))) + "To" + result.name;
+        string path = EditorUtility.SaveFilePanelInProject("Save Recipy", rname, "asset", "Save recipy", "Assets/Scripptable Objects/Recipys");
+        if (path == "")
+        {
+            return;
+        }
+        var r = ScriptableObject.CreateInstance<Recipe>();
+        r.input = objects.Select(s => s as AlchemyItem).ToArray();
+
+        r.output = result as AlchemyItem;
+        AssetDatabase.CreateAsset(r, path);
     }
 }
