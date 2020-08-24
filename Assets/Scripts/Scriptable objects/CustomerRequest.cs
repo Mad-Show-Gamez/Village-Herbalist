c96af65064b4336bcd2e80ed1008ccb4a0e324fb
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-08-24 19:05:45 +0100

    add avatar

--- Assets/Scripts/Scriptable objects/CustomerRequest.cs
+++ Assets/Scripts/Scriptable objects/CustomerRequest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New CustomerRequest", menuName = "CustomerRequest")]
 public class CustomerRequest : ScriptableObject
 {
+    [SerializeField] internal Sprite customerAvatar;
     [SerializeField] internal string description;
     [SerializeField] internal List<AlchemyItem> acceptedAI;
     [SerializeField] internal List<CustomerRequest> newRequests;
