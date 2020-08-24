e5bc785815eac3a1b5923da5ef7899fbfbd7f8db
ShoCo2 <coutu.calvin@gmail.com>
2020-08-24 00:48:49 -0400

    Change custrequest/manager

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
@@ -11,8 +10,10 @@ public class CustomerManager : MonoBehaviour
     [SerializeField] GameObject CustomerText;
     [SerializeField] GameObject Canvas;
 
-    int MaxCustomerCount = 1;
+    int MaxCustomerCount = 3;
     int CurrentCustomerCount = 0;
+    float CustomerSpawnCooldown = 5;
+    float SpawnTime = 0;
 
     void Start()
     {
@@ -21,13 +22,27 @@ public class CustomerManager : MonoBehaviour
 
     void Update()
     {
-        if (CurrentCustomerCount < MaxCustomerCount)
+        SpawnTime += Time.deltaTime;
+        if (CurrentCustomerCount < MaxCustomerCount && SpawnTime >= CustomerSpawnCooldown)
         {
             CurrentCustomerCount++;
             Customer newCustomer;
             newCustomer = Instantiate(CustomerPrefab, Vector3.zero, Quaternion.identity).GetComponent<Customer>();
-            CustomerRequest newRequest = AvailableRequests.ElementAt(Random.Range(0, AvailableRequests.Count));
-            newCustomer.Initialize(newRequest, Canvas, CustomerText);
+            newCustomer.Initialize(this, GetNewRequest(), Canvas, CustomerText);
+            SpawnTime = 0;
         }
+    }
+
+    CustomerRequest GetNewRequest()
+    {
+        CustomerRequest newRequest = AvailableRequests[Random.Range(0, AvailableRequests.Count)];
+        AvailableRequests.Remove(newRequest);
+        return newRequest;
+    }
+
+    public void DestroyCustomer(Customer customer)
+    {
+        Destroy(customer);
+        CurrentCustomerCount--;
     }
 }
