a7a0f40b8f395e4ab3f4ec153ab6c0b0f06ec4ac
ShoCo2 <coutu.calvin@gmail.com>
2020-08-24 06:04:34 -0400

    Implemented drag and drop from UI

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -1,24 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class InventoryManager : MonoBehaviour
 {
+    int DefaultSlotCount = 9;
+    [SerializeField] GameObject onClick;
+    [SerializeField] GameObject GridLayoutGO;
+    [SerializeField] GameObject ItemBox;
     [SerializeField] GameObject AlchemyPrefab;
     [SerializeField] Inventory inventory;
+    [SerializeField] ItemSlot[] itemSlots;
+    [SerializeField] ItemSlot CurrentSlot;
+    GameObject selectedObject;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (CurrentSlot != null && CurrentSlot.AI != null)
+            {
+                GetItem(CurrentSlot.AI);
+                UpdateCurrentSlot(CurrentSlot);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (selectedObject != null && CurrentSlot != null && CurrentSlot.AI == null) AddItem(selectedObject);
+        }
+
+        if (onClick.transform.childCount > 0) selectedObject = onClick.transform.GetChild(0).gameObject;
+        else selectedObject = null;
+    }
+
+    void Start()
+    {
+        itemSlots = new ItemSlot[DefaultSlotCount];
+
+        for (int i = 0; i < DefaultSlotCount; i++)
+        {
+            Debug.Log(i);
+            GameObject newSlot = Instantiate(ItemBox, Vector3.zero, Quaternion.identity);
+            newSlot.transform.SetParent(GridLayoutGO.transform);
+            newSlot.transform.localScale = Vector3.one;
+            itemSlots[i] = newSlot.GetComponent<ItemSlot>();
+            if (i < inventory.contents.Count) itemSlots[i].UpdateSlot(inventory.contents[i]);
+            else itemSlots[i].UpdateSlot();
+        }
+    }
+
+    public void UpdateCurrentSlot(ItemSlot slot)
+    {
+        CurrentSlot = slot;
+    }
 
     public void AddItem(GameObject newItem)
     {
-        AlchemyItem alItem = newItem.GetComponent<AlchemyItem>();
-        inventory.contents.Add(alItem);
-        Destroy(alItem);
+        CurrentSlot.UpdateSlot(newItem.GetComponent<AlchemyItemMB>().item.type);
+        Destroy(newItem);
     }
 
     public void GetItem(AlchemyItem newItem)
     {
-        inventory.contents.Remove(newItem);
-        AlchemyItemMB alMB = Instantiate(AlchemyPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity).GetComponent<AlchemyItemMB>();
+        AlchemyItemMB alMB = Instantiate(AlchemyPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward, Quaternion.identity).GetComponent<AlchemyItemMB>();
         alMB.item.type = newItem;
         alMB.populateGO();
+        CurrentSlot.UpdateSlot();
     }
 }
---
+++ Assets/ItemSlot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Image slotImage;
+    InventoryManager manager;
+    [NaughtyAttributes.ReadOnly][SerializeField]internal AlchemyItem AI;
+
+    void Start()
+    {
+        manager = transform.parent.parent.parent.parent.GetComponent<InventoryManager>();
+    }
+
+    public void UpdateSlot(AlchemyItem AI = null)
+    {
+        this.AI = AI;
+        if (AI != null)
+        {
+            slotImage.enabled = true;
+            slotImage.sprite = AI.Sprite;
+        }
+        else
+        {
+            slotImage.enabled = false;
+            slotImage.sprite = null;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        manager.UpdateCurrentSlot(this);   
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        manager.UpdateCurrentSlot(null);
+    }
+}
